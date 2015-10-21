e2da7c109a11324af79352f723776fd10d416145
chendong <chendong0316@qq.com>
2015-10-21 21:19:39 +0800

    提交了陈东同学的测试单元1

---
+++ Lab3/105032013095陈东/测试单元1/MathsOperators - Complete/Calculator/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMath
+{
+    public class Calculator
+    {
+        public Calculator(char op)
+        {
+            this.op = op;
+        }
+        private char __op;
+        public char op
+        {
+            get
+            {
+                return __op;
+            }
+            set
+            {
+                if (value == '+' || value == '-' ||
+                    value == '*' || value == '/')
+                {
+                    __op = value;
+                }
+                else
+                {
+                    __op = '\0';
+                }
+            }
+        }
+
+        public double calc(double a, double b)
+        {
+            switch(op)
+            {
+                case '+':
+                    return a + b;
+                case '-':
+                    return a - b;
+                case '*':
+                    return a * b;
+                case '/':
+                    return a / b;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ Lab3/105032013095陈东/测试单元1/MathsOperators - Complete/CalculatorUnitTest/AssemblyInfo.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyMath;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMath.Tests
+{
+    [TestClass()]
+    public class AssemblyInfo
+    {
+        [TestMethod()]
+        public void CalculatorTest()
+        {
+//            Assert.Fail();
+        }
+
+        /// <summary>
+        /// 测试加法
+        /// </summary>
+        [TestMethod()]
+        public void addTest()
+        {
+            Calculator calc = new Calculator('+');
+            double result = calc.calc(3.3, 5.5);
+            Assert.IsTrue(result == 8.8);
+        }
+
+        /// <summary>
+        /// 测试减法
+        /// </summary>
+        [TestMethod()]
+        public void subTest()
+        {
+            Calculator calc = new Calculator('-');
+            double result = calc.calc(3.3, 5.5);
+            Assert.IsTrue(result == -2.2);
+        }
+
+        /// <summary>
+        /// 测试乘法
+        /// </summary>
+        [TestMethod()]
+        public void multiTest()
+        {
+            Calculator calc = new Calculator('*');
+            double result = calc.calc(3.3, 5.5);
+            Assert.IsTrue(result == 18.15);
+        }
+
+        /// <summary>
+        /// 测试除法
+        /// </summary>
+        [TestMethod()]
+        public void divTest1()
+        {
+            Calculator calc = new Calculator('/');
+            double result = calc.calc(3.3, 5.5);
+            Assert.IsTrue(result == 0.6);
+        }
+
+        /// <summary>
+        /// 测试除法（分母为0）
+        /// </summary>
+        [TestMethod()]
+        public void divTest2()
+        {
+            Calculator calc = new Calculator('/');
+            double result = calc.calc(3.3, 0);
+            Assert.IsTrue(Double.IsInfinity(result));
+        }
+    }
+}
