54b8fb8b295d594232a55e24e7699f87f5a1b4d2
chendong <chendong0316@qq.com>
2015-10-21 23:53:32 +0800

    提交了陈东同学和毛磊同学的实验报告

---
+++ Lab3/105032013095陈东/单元测试2/GoodEmail/GoodEmail/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace GoodEmail
+{
+    public class Program
+    {
+        public static bool vertify(String email)
+        {
+            string strRegex = @"^([a-zA-Z0-9_-]+)@([a-zA-Z0-9_-]+).([a-zA-Z0-9_-]+)$";
+            Regex re = new Regex(strRegex);
+            if (re.IsMatch(email))
+                return (true);
+            else
+                return (false);
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Lab3/105032013095陈东/单元测试2/GoodEmail/GoodEmailTests/ProgramTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GoodEmail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodEmail.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void vertifyTest1()
+        {
+            bool result = Program.vertify("11@aa.com");
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void vertifyTest2()
+        {
+            bool result = Program.vertify("");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod()]
+        public void vertifyTest3()
+        {
+            bool result = Program.vertify("11@.com");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod()]
+        public void vertifyTest4()
+        {
+            bool result = Program.vertify("11aa.com");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod()]
+        public void vertifyTest5()
+        {
+            bool result = Program.vertify("11@aacom");
+            Assert.IsFalse(result);
+        }
+    }
+}
